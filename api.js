a335b8416f049e0d4f744bb28d3072e01d2c8ea4
samitkumarpatel-work <SamitKumar.Patel@maersk.com>
2024-04-29 16:33:45 +0200

    db timeout

--- api.js
+++ api.js
@@ -13,7 +13,7 @@ app.use(cors())
 //db
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://127.0.0.1:27017", { dbName: "person", user: "root", pass: "example"})
+mongoose.connect("mongodb://127.0.0.1:27017", { dbName: "person", user: "root", pass: "example", connectTimeoutMS: 1000, socketTimeoutMS: 1000})
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...', err))
 
